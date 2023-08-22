d1155894dbaa85799c8161cc0b6c6d0e265f298c
ICHINOSE Shogo <shogo82148@gmail.com>
2023-08-22 23:27:57 +0900

    implement bilinear resizing

---
+++ resize/bilinear_test.go
@@ -0,0 +1,29 @@
+package resize
+
+import (
+	"image"
+	"testing"
+
+	"github.com/shogo82148/go-imaging/internal/testutils"
+)
+
+func TestBiLinear(t *testing.T) {
+	dst := image.NewNRGBA64(image.Rect(0, 0, 4, 4))
+	src := testutils.LoadPNM(`P3
+2 2
+65535
+0     0 0  65535 65535 65535
+32768 0 0      0 32768     0
+`)
+	BiLinear(dst, src)
+
+	want := testutils.LoadPNM(`P3
+4 4
+65535
+0     0 0  21845 21845 21845  43690 43690 43690  65535 65535 65535
+10923 0 0  21845 18204 14563  32768 36408 29127  43690 54613 43690
+21845 0 0  21845 14563  7282  21845 29127 14563  21845 43690 21845
+32768 0 0  21845 10923     0  10923 21845     0      0 32768     0
+`)
+	testutils.Compare(t, dst, want)
+}
