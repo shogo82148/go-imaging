fbac11c37c2c8f66b163634c4edb6538f109bbd8
ICHINOSE Shogo <shogo82148@gmail.com>
2023-09-24 04:38:02 +0900

    Revert "remove icc.FuzzDecode"
    This reverts commit 34935c9dcce8e6b67468af73db606b410905522f.

---
+++ icc/fuzz_test.go
@@ -0,0 +1,56 @@
+package icc
+
+import (
+	"os"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func FuzzDecode(f *testing.F) {
+	testdata := []string{
+		"testdata/D65_XYZ.icc",
+		"testdata/iPhone12Pro.icc",
+	}
+	for _, path := range testdata {
+		data, err := os.ReadFile(path)
+		if err != nil {
+			f.Error(err)
+		}
+		f.Add(data)
+	}
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		p0, err := Decode(data)
+		if err != nil {
+			return
+		}
+
+		encoded0, err := Encode(p0)
+		if err != nil {
+			t.Fatalf("failed to encode: %v", err)
+		}
+
+		p1, err := Decode(encoded0)
+		if err != nil {
+			t.Fatalf("failed to decode: %v", err)
+		}
+
+		// ignore differences in the profile size
+		p0.Size = 0
+		p1.Size = 0
+
+		if diff := cmp.Diff(p0, p1); diff != "" {
+			t.Errorf("mismatch (-want +got):\n%s", diff)
+		}
+
+		encoded1, err := Encode(p1)
+		if err != nil {
+			t.Fatalf("failed to encode: %v", err)
+		}
+		if diff := cmp.Diff(encoded0, encoded1); diff != "" {
+			t.Errorf("mismatch (-want +got):\n%s", diff)
+		}
+
+	})
+}
