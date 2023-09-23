e2d6c52a7e266026fec01042cce05a886516ad90
ICHINOSE Shogo <shogo82148@gmail.com>
2023-09-24 04:43:22 +0900

    remove seed

--- icc/fuzz_test.go
+++ icc/fuzz_test.go
@@ -1,25 +1,12 @@
 package icc
 
 import (
-	"os"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
 
 func FuzzDecode(f *testing.F) {
-	testdata := []string{
-		"testdata/D65_XYZ.icc",
-		"testdata/iPhone12Pro.icc",
-	}
-	for _, path := range testdata {
-		data, err := os.ReadFile(path)
-		if err != nil {
-			f.Error(err)
-		}
-		f.Add(data)
-	}
-
 	f.Fuzz(func(t *testing.T, data []byte) {
 		p0, err := Decode(data)
 		if err != nil {
