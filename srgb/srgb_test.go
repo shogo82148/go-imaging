3ce0cbf31b389680ee918c0c8254383ba7c6bee9
ICHINOSE Shogo <shogo82148@gmail.com>
2023-09-24 14:53:21 +0900

    rename Decode to Linearize

--- srgb/srgb_test.go
+++ srgb/srgb_test.go
@@ -8,27 +8,27 @@ import (
 func BenchmarkDecode_RGBA(b *testing.B) {
 	input := image.NewRGBA(image.Rect(0, 0, 512, 512))
 	for i := 0; i < b.N; i++ {
-		Decode(input)
+		Linearize(input)
 	}
 }
 
 func BenchmarkDecode_RGBA64(b *testing.B) {
 	input := image.NewRGBA64(image.Rect(0, 0, 512, 512))
 	for i := 0; i < b.N; i++ {
-		Decode(input)
+		Linearize(input)
 	}
 }
 
 func BenchmarkDecode_NRGBA(b *testing.B) {
 	input := image.NewNRGBA(image.Rect(0, 0, 512, 512))
 	for i := 0; i < b.N; i++ {
-		Decode(input)
+		Linearize(input)
 	}
 }
 
 func BenchmarkDecode_NRGBA64(b *testing.B) {
 	input := image.NewNRGBA64(image.Rect(0, 0, 512, 512))
 	for i := 0; i < b.N; i++ {
-		Decode(input)
+		Linearize(input)
 	}
 }
