0fee6fe80c16d1a8d49e7491a8570e03f8cc8f08
ICHINOSE Shogo <shogo82148@gmail.com>
2023-09-24 14:55:13 +0900

    rename benchmark name

--- srgb/srgb_test.go
+++ srgb/srgb_test.go
@@ -5,28 +5,28 @@ import (
 	"testing"
 )
 
-func BenchmarkDecode_RGBA(b *testing.B) {
+func BenchmarkLinearize_RGBA(b *testing.B) {
 	input := image.NewRGBA(image.Rect(0, 0, 512, 512))
 	for i := 0; i < b.N; i++ {
 		Linearize(input)
 	}
 }
 
-func BenchmarkDecode_RGBA64(b *testing.B) {
+func BenchmarkLinearize_RGBA64(b *testing.B) {
 	input := image.NewRGBA64(image.Rect(0, 0, 512, 512))
 	for i := 0; i < b.N; i++ {
 		Linearize(input)
 	}
 }
 
-func BenchmarkDecode_NRGBA(b *testing.B) {
+func BenchmarkLinearize_NRGBA(b *testing.B) {
 	input := image.NewNRGBA(image.Rect(0, 0, 512, 512))
 	for i := 0; i < b.N; i++ {
 		Linearize(input)
 	}
 }
 
-func BenchmarkDecode_NRGBA64(b *testing.B) {
+func BenchmarkLinearize_NRGBA64(b *testing.B) {
 	input := image.NewNRGBA64(image.Rect(0, 0, 512, 512))
 	for i := 0; i < b.N; i++ {
 		Linearize(input)
