6d6f9746e36aa34f7cb47c36f5ea2d74e4597edd
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-05 22:37:16 +0900

    add more test exif files

--- exif/encode_test.go
+++ exif/encode_test.go
@@ -9,29 +9,40 @@ import (
 )
 
 func TestEncode(t *testing.T) {
-	f, err := os.Open("testdata/senkakuwan.exif")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer f.Close()
-
-	tiff0, err := Decode(f)
-	if err != nil {
-		t.Fatal(err)
+	tests := []string{
+		"testdata/fireworks.exif",
+		"testdata/flower.exif",
+		"testdata/sado-gold-mine.exif",
+		"testdata/senkakuwan.exif",
 	}
 
-	var buf bytes.Buffer
-	err = Encode(&buf, tiff0)
-	if err != nil {
-		t.Fatal(err)
-	}
+	for _, test := range tests {
+		t.Run(test, func(t *testing.T) {
+			f, err := os.Open(test)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer f.Close()
 
-	tiff1, err := Decode(&buf)
-	if err != nil {
-		t.Fatal(err)
-	}
+			tiff0, err := Decode(f)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	if diff := cmp.Diff(tiff0, tiff1); diff != "" {
-		t.Errorf("Encode() mismatch (-want +got):\n%s", diff)
+			var buf bytes.Buffer
+			err = Encode(&buf, tiff0)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			tiff1, err := Decode(&buf)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if diff := cmp.Diff(tiff0, tiff1); diff != "" {
+				t.Errorf("Encode() mismatch (-want +got):\n%s", diff)
+			}
+		})
 	}
 }
